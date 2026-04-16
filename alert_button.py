6378c12f438e7b9c67bd180025c4d60b9150173b
Ronidanish <anwar4@ensign.edu>
2026-04-16 16:00:07 -0600

    Update alert_button.py
    added telegram chat function

--- alert_button.py
+++ alert_button.py
@@ -1,14 +1,45 @@
+
 import time
 import RPi.GPIO as GPIO
+import requests
+
+
+TOKEN: “8780387298:AAE4lDtkcighsE6Md"
+CHAT_ID = "603950****"
+
+
+def send_telegram_msg(text):
+    url = f"https://api.telegram.org/bot8780387298:AAE4lDtkcighsE6Md_ID6uuNOMHMmA/sendmessage"
+    payload = {"603950669": CHAT_ID, "text": text}
+    try:
+        requests.post(url, data=payload, timeout=5)
+    except Exception as e:
+        print(f"Connection Error: {e}")
+
+
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 button_pressed = False
 
-while True:
-    if GPIO.input(7) == GPIO.HIGH and not button_pressed:
-        print("Someone pressed the alert button!")
-        button_pressed = True
-    elif GPIO.input(7) == GPIO.LOW:
-        button_pressed = False
-    time.sleep(0.1)
+print("System is running!")
 
+
+try:
+    while True:
+
+        if GPIO.input(7) == GPIO.HIGH:
+            if not button_pressed:
+                print("Button Pressed! Sending Telegram...")
+                send_telegram_msg (“ someone pushed the button")
+                button_pressed = True
+        else:
+
+            button_pressed = False
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:
+    print("\nExiting the program. Have a nice day!")
+    GPIO.cleanup()
